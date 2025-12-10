22c16882d6239efb7decd0f4d12ffe5038ceb071
Aritz Ugarte <aritz.ugarte@alumni.mondragon.edu>
2025-12-10 11:06:28 +0100

    g

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
           System.out.println("Bye world!");
-             System.out.println("Be world!");
+             System.out.println("Be xzcxczxzxworld!");
     }
 }
